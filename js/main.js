7c76061d90bee5d3a713d5ebbcf2b22908e6f92e
HeleneElena <heleneebert1@gmail.com>
2022-08-24 14:10:43 +0200

    commit3

--- js/main.js
+++ js/main.js
@@ -1,17 +1,50 @@
 const btnMen = document.querySelector('.header__btn-gender_men'),
       btnWomen = document.querySelector('.header__btn-gender_women'),
-      body = document.body;
+      body = document.body,
+      cardImg = document.querySelector('.card__image'),
+      cardText = document.querySelector('.card__text');
+
+// меняем фон с женского на мужской и обратно
+const state = {
+    gender: body.classList.contains('women') ? 'women' : 'men',
+};
+
+const getRandomForArr = (arr) => {
+    const randomNumber = Math.floor(Math.random() * arr.length);
+    return arr[randomNumber];
+};
+
+// функция для получения данных
+const getData = () => fetch('db.json').then((response) => response.json());
+
+const getDataToCard = () => {
+     getData().then(data => {
+        state.text = getRandomForArr(data.text[state.gender]);
+        state.photo = getRandomForArr(data.photo[state.gender]);
+        cardImg.src = `image/${state.photo}`;
+        cardText.innerHTML = state.text.replaceAll('\n', '<br>');
+     });
+};
 
 const changeToMen = () => {
-    body.classList.add('men');
-    body.classList.remove('women');
+    if (state.gender !== 'men') {
+        body.classList.add('men');
+        body.classList.remove('women');
+        state.gender = 'men';
+        getDataToCard();
+    }
+    
 };
 
 const changeToWomen = () => {
-    body.classList.add('women');
-    body.classList.remove('men');
+    if (state.gender !== 'women') {
+        body.classList.add('women');
+        body.classList.remove('men');
+        state.gender = 'women';
+        getDataToCard();
+    }   
 };
 
 btnMen.addEventListener('click', changeToMen);
-
 btnWomen.addEventListener('click', changeToWomen);
+getDataToCard();
